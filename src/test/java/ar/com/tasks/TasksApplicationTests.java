f01c09aa16ff58af388ed526dedd08a08122c250
Joaquin Cibanal <joaquincibanal@gmail.com>
2022-07-15 14:52:37 -0300

    first commit

---
+++ src/test/java/ar/com/tasks/TasksApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
